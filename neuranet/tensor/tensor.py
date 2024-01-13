2c19b9470f94f17e806da520261adf850461bd1a
JamorMoussa <moussajamorsup@gmail.com>
2024-01-13 01:07:37 +0100

    C#3: add neuranet lib, add Tensor.

---
+++ neuranet/tensor/tensor.py
@@ -0,0 +1,57 @@
+import numpy as np 
+
+
+
+class Tensor(np.ndarray):
+    """
+        Tensor: class represents multi-dimension array in NeuraNet library, It's extends for the numpy 
+                ndarray base class, which means that has all features supported by the numpy.adarray. 
+                but will include new features in the feature release like 'autograd'.
+    """
+    def __new__(cls, input: 'Tensor') -> 'Tensor':
+        """
+            This method will create a Tensor.
+            
+            Args:
+                input: is Tensor, list, tuple, np.ndarray. These are the only types supported in the current version.
+            
+            Returns:
+                Returns a Tensor object.
+            
+            Note: 
+                Every Tensor has at least two dimensions, even if the input is 1-dimensional.
+                
+            Example:
+                >>> import neuranet as nnt
+                >>> a = nnt.Tensor([1, 2, 3])
+                >>> a
+                Tensor([[1, 2, 3]]) 
+                >>> a.shape 
+                (1, 3)
+                >>> a.T
+                Tensor([[1],
+                        [2],
+                        [3]])
+        """
+        
+        if not isinstance(input, (Tensor, list, tuple, np.ndarray)): 
+            raise ValueError(f"the 'input' attributes must be list, tuple, numpy.ndarray. But '{input.__class__.__name__}' is given") 
+        
+        if not isinstance(input, np.ndarray): input = np.array(input)
+        if input.ndim ==1 : input = input.reshape(1, -1)
+        
+        obj = np.asanyarray(input).view(cls)
+
+        return obj
+
+    @staticmethod
+    def rand(*shape: tuple[int]) -> 'Tensor':
+        return Tensor(np.random.rand(*shape))
+
+    @staticmethod
+    def zeros(*shape: tuple[int]) -> 'Tensor':
+        return Tensor(np.zeros(shape))
+    
+    @staticmethod
+    def dot(t1: 'Tensor', t2: 'Tensor') -> 'Tensor':
+        return Tensor(np.dot(t1, t2))
---
+++ test.py
@@ -0,0 +1,6 @@
+import neuranet as nnt 
+import numpy as np
+
+a = nnt.Tensor([1, 2 , 3])
+
+print(help(a))
