12df958c2f0863592a69c3a353585fa23fe7a35b
JamorMoussa <moussajamorsup@gmail.com>
2024-01-28 21:50:33 +0100

    C#28: add add_layer in sequential, change the constuctor to allow no argument

--- neuranet/nn/modules/container.py
+++ neuranet/nn/modules/container.py
@@ -1,15 +1,18 @@
 from ..base import Module, Layer
 from neuranet import Tensor
-from typing import List
+from typing import List, Tuple
 
 __all__ = ["Sequential"]
 
 class Sequential(Module):
 
-    def __init__(self, layers: List[Layer]):
-        super().__init__()
+    def __init__(self, *layers: List[Layer] | Tuple[Layer]) -> None:
+        super(Sequential, self).__init__()
 
         for layer in layers: setattr(self, layer.__class__.__name__, layer)
+
+    def add_layer(self, layer: Layer) -> None:
+        setattr(self, layer.__class__.__name__, layer)
 
     def forward(self, input: Tensor) -> Tensor:
         out = input
