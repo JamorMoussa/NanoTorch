289bcf8465a03555a101ebb3e1c70ef4665ad088
ANAS NOURI <anasnouriia@gmail.com>
2024-04-28 23:11:50 +0100

    add sigmoid layer

--- nanotorch/nn/functional.py
+++ nanotorch/nn/functional.py
@@ -1,7 +1,13 @@
 from nanotorch import Tensor, where
+import numpy as np
 
-__all__ = ["relu"]
+__all__ = ["relu","sigmoid"]
 
 
 def relu(input: Tensor) -> Tensor:
     return where(input > 0, input, 0)
+
+
+def sigmoid(input: Tensor)-> Tensor:
+    return Tensor(1/(1 + np.exp(-input)))
+
--- nanotorch/nn/modules/activations.py
+++ nanotorch/nn/modules/activations.py
@@ -2,7 +2,7 @@ from nanotorch.nn.base import Activation
 import nanotorch.nn.functional as F
 from nanotorch import where 
 
-__all__ = ["ReLU"]
+__all__ = ["ReLU", "Sigmoid"]
 
 
 class ReLU(Activation):
@@ -12,4 +12,14 @@ class ReLU(Activation):
         super(ReLU, self).__init__(
             F.relu,
             lambda tensor: where(tensor <= 0, 0, 1) 
+        )
+
+
+class Sigmoid(Activation):
+    
+    def __init__(self) -> None:
+
+        super(Sigmoid, self).__init__(
+            active_func = F.sigmoid,
+            active_prime = lambda input: F.sigmoid(input)*(1 - F.sigmoid(input)) 
         )
