8cc560a10d5b825b719281b4622b49f7b06d90df
JamorMoussa <moussajamorsup@gmail.com>
2024-05-01 22:17:00 +0100

    add input_as decorator (#13)

--- nanotorch/nn/functional.py
+++ nanotorch/nn/functional.py
@@ -1,17 +1,18 @@
 from nanotorch import Tensor, where
+from nanotorch.utils._decorators import input_as
 import numpy as np
 
 __all__ = ["relu", "relu_prime", "sigmoid",]
 
-
+@input_as(DType = Tensor)
 def relu(input: Tensor) -> Tensor:
     return where(input > 0, input, 0)
 
-
+@input_as(DType = Tensor)
 def relu_prime(input: Tensor) -> Tensor:
     return where(input > 0 , 1, 0) 
 
-
+@input_as(DType = Tensor)
 def sigmoid(input: Tensor)-> Tensor:
     return Tensor(1/(1 + np.exp(-input)))
 
